b4f990884d6db8c77d08eb0c14bc836d1b8c6c9d
Jahid hasan <jahidhasan@Jahids-MacBook-Air.local>
2026-04-15 01:20:40 +0600

    timeline page sortby text ,call and vedio functionlity completed

--- src/pages/TimeLine/TimeLine.jsx
+++ src/pages/TimeLine/TimeLine.jsx
@@ -18,9 +18,17 @@ const TimeLine = () => {
 
     const { timeLineContect } = useContext(TimeLineContect)
     const [search, setSearch] = useState('')
-    
+
 
     const filterSearch = timeLineContect.filter(contect => contect.name.toLowerCase().includes(search.toLowerCase()))
+
+
+    const [sortText, setSortText] = useState('')
+    const fillterbytext = [...filterSearch]
+
+    if (sortText) {
+        fillterbytext.sort((a, b) => (b.status === sortText) - (a.status === sortText));
+    }
 
 
 
@@ -31,10 +39,9 @@ const TimeLine = () => {
                 <div className="dropdown dropdown-end">
                     <div tabIndex={0} role="button" className="btn m-1 text-neutral/50">Filter by <FaAngleDown /></div>
                     <ul tabIndex="-1" className="dropdown-content menu bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm">
-                        <li><a>Filter by timeline</a></li>
-                        <li><a>Filter by text</a></li>
-                        <li><a>Filter by call</a></li>
-                        <li><a>Filter by video</a></li>
+                        <li onClick={() => setSortText('text')}><a>Filter by text</a></li>
+                        <li onClick={() => setSortText('call')}><a>Filter by call</a></li>
+                        <li onClick={() => setSortText('video')}><a>Filter by video</a></li>
                     </ul>
                 </div>
 
@@ -51,12 +58,12 @@ const TimeLine = () => {
 
 
             {
-                filterSearch.length === 0 ?
+                fillterbytext.length === 0 ?
                     <div className='text-center text-neutral/50 bg-base-100 shadow-sm rounded-sm py-20'>
                         <p>No contacts yet!</p>
                     </div> :
 
-                    filterSearch.map(contect =>
+                    fillterbytext.map(contect =>
                         <div className='' key={contect.id}>
 
                             {contect.status === 'call' && (<div className='flex gap-2 bg-base-100 shadow-sm py-4 px-4 items-center rounded-sm mb-2' key={contect.id}>
@@ -72,7 +79,7 @@ const TimeLine = () => {
                             {contect.status === 'text' && (<div className='flex gap-2 bg-base-100 shadow-sm py-4 px-4 items-center rounded-sm mb-2' key={contect.id}>
                                 <img className='w-10 h-10' src={textIcon} alt="" />
                                 <div className='space-1-2'>
-                                    <h2><span className='font-semibold text-[1.10rem]'>Call</span> <span className='text-neutral/50'>With {contect.name}</span></h2>
+                                    <h2><span className='font-semibold text-[1.10rem]'>Text</span> <span className='text-neutral/50'>With {contect.name}</span></h2>
                                     <p className='text-neutral/50'>{dateFormat()}</p>
                                 </div>
                             </div>)}
@@ -81,7 +88,7 @@ const TimeLine = () => {
                             {contect.status === 'video' && (<div className='flex gap-2 bg-base-100 shadow-sm py-4 px-4 items-center rounded-sm mb-2' key={contect.id}>
                                 <img className='w-10 h-10' src={videoIcon} alt="" />
                                 <div className='space-1-2'>
-                                    <h2><span className='font-semibold text-[1.10rem]'>Call</span> <span className='text-neutral/50'>With {contect.name}</span></h2>
+                                    <h2><span className='font-semibold text-[1.10rem]'>Video</span> <span className='text-neutral/50'>With {contect.name}</span></h2>
                                     <p className='text-neutral/50'>{dateFormat()}</p>
                                 </div>
                             </div>)}
